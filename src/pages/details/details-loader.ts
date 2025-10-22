527ce4aed7d9600a8996530efe2c882e55590d70
Shakir <shakirkhan72727@gmail.com>
2025-10-22 23:10:58 +0500

    render package details

--- src/pages/details/details-loader.ts
+++ src/pages/details/details-loader.ts
@@ -1,10 +1,18 @@
+import type { PackageDetails } from "./../../api/types/packageDetails";
 import { getPackage } from "../../api/queries/getPackage";
 import type { Params } from "react-router-dom";
 
 interface LoaderArgs {
   params: Params;
 }
-export const detailsLoader = async ({ params }: LoaderArgs) => {
+
+export interface DetailsLoaderResult {
+  details: PackageDetails;
+}
+
+export const detailsLoader = async ({
+  params,
+}: LoaderArgs): Promise<DetailsLoaderResult> => {
   const { name } = params;
 
   if (!name) {
--- src/pages/details/details-page.tsx
+++ src/pages/details/details-page.tsx
@@ -1,7 +1,27 @@
 import { useLoaderData } from "react-router-dom";
+import type { DetailsLoaderResult } from "./details-loader";
 
 export const DetailsPage = () => {
-  const data = useLoaderData();
-  console.log(data);
-  return <div>DetailsPage</div>;
+  const { details } = useLoaderData() as DetailsLoaderResult;
+
+  return (
+    <div className="space-y-4">
+      <h1 className="text-3xl font-bold my-4">{details.name}</h1>
+
+      <div>
+        <h3 className="text-lg font-bold">Description</h3>
+        <div className="p-3 bg-gray-200 rounded">{details.description}</div>
+      </div>
+
+      <div>
+        <h3 className="text-lg font-bold">License</h3>
+        <div className="p-3 bg-gray-200 rounded">{details.license}</div>
+      </div>
+
+      <div>
+        <h3 className="text-lg font-bold">Author</h3>
+        <div className="p-3 bg-gray-200 rounded">{details.author.name}</div>
+      </div>
+    </div>
+  );
 };
